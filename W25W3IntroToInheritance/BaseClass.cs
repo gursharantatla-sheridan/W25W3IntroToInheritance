3b378e789b472edd55523d55c699a2371ada7d9c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-22 15:52:30 -0500

    constructor calls

--- W25W3IntroToInheritance/BaseClass.cs
+++ W25W3IntroToInheritance/BaseClass.cs
@@ -8,18 +8,33 @@ namespace W25W3IntroToInheritance
 {
     public class BaseClass
     {
-        private int myPrivateVar;
-        protected int myProtectedVar;
-        public int myPublicVar;
+        //private int myPrivateVar;
+        //protected int myProtectedVar;
+        //public int myPublicVar;
+
+        public BaseClass()
+        {
+            Console.WriteLine("Base class constructor called");
+        }
+
+        public BaseClass(string msg)
+        {
+            Console.WriteLine("Base class constructor with message: " + msg);
+        }
     }
 
     public class DerivedClass : BaseClass
     {
-        public DerivedClass()
+        public DerivedClass() : base("hello")
         {
-            // myPrivateVar = 1;
-            myProtectedVar = 2;
-            myPublicVar = 3;
+            Console.WriteLine("Derived class constructor called");
         }
+
+        //public DerivedClass()
+        //{
+        //    // myPrivateVar = 1;
+        //    myProtectedVar = 2;
+        //    myPublicVar = 3;
+        //}
     }
 }
